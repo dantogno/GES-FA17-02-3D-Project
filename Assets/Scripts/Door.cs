7c888177017343419027a12e57693c2c7faf8e66
David Antognoli <David.Antognoli@iam.local>
2017-11-27 15:08:08 -0600

    working door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IActivatable {
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void DoActivate()
+    {
+        animator.SetBool("isOpen", true);
+    }
+}
