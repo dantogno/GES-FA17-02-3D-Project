5d36c5262f81cfe34d5881e90d878255dabb582c
David Antognoli <David.Antognoli@iam.local>
2017-12-04 14:33:55 -0600

    inventory menu built, shows up when you press esc

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject inventoryMenuPanel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        HideMenu();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HandleInput();	
+	}
+
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            ShowMenu();
+        }
+    }
+
+    private void ShowMenu()
+    {
+        inventoryMenuPanel.SetActive(true);
+    }
+
+    private void HideMenu()
+    {
+        inventoryMenuPanel.SetActive(false);
+    }
+}
