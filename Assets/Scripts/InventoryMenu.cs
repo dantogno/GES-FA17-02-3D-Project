417ab9f37572d3c3b69fa6d6008e953b985dac0c
David Antognoli <David.Antognoli@iam.local>
2017-12-04 14:44:57 -0600

    cursor is updating correctly, menu shows and hides

--- Assets/Scripts/InventoryMenu.cs
+++ Assets/Scripts/InventoryMenu.cs
@@ -8,6 +8,14 @@ public class InventoryMenu : MonoBehaviour {
     [SerializeField]
     private GameObject inventoryMenuPanel;
 
+    private bool IsMenuOpen
+    {
+        get
+        {
+            return inventoryMenuPanel.activeSelf;
+        }
+    }
+
 	// Use this for initialization
 	void Start ()
     {
@@ -24,17 +32,36 @@ public class InventoryMenu : MonoBehaviour {
     {
         if (Input.GetButtonDown("Cancel"))
         {
-            ShowMenu();
+            if (IsMenuOpen)
+                HideMenu();
+            else
+                ShowMenu();
         }
     }
 
     private void ShowMenu()
     {
         inventoryMenuPanel.SetActive(true);
+        UpdateCursor();
     }
 
     private void HideMenu()
     {
         inventoryMenuPanel.SetActive(false);
+        UpdateCursor();
+    }
+
+    private void UpdateCursor()
+    {
+        if (IsMenuOpen)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
